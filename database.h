01e6a2a11a4c83858da78a5387d41a51e7acc218
Maksim Kl <GuruHacker@yandex.ru>
2023-10-27 23:56:25 +0300

    Initial commit

---
+++ database.h
@@ -0,0 +1,78 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+#include <QTableWidget>
+#include <QObject>
+#include <QSqlError>
+
+#include <QSqlDatabase>
+#include <QSqlQuery>
+
+#include <QSqlQueryModel>
+#include <QSqlTableModel>
+
+#define POSTGRE_DRIVER "QPSQL"
+#define DB_NAME "MyDB"
+
+//Количество полей данных необходимых для подключения к БД
+#define NUM_DATA_FOR_CONNECT_TO_DB 5
+
+//Перечисление полей данных
+enum fieldsForConnect{
+    hostName = 0,
+    dbName = 1,
+    login = 2,
+    pass = 3,
+    port = 4
+};
+
+//Типы запросов
+enum requestType{
+
+    requestAllFilms = 1,
+    requestComedy   = 2,
+    requestHorrors  = 3
+
+};
+
+
+
+class DataBase : public QObject
+{
+    Q_OBJECT
+
+public:
+    explicit DataBase(QObject *parent = nullptr);
+    ~DataBase();
+
+    void AddDataBase(QString driver, QString nameDB = "");
+    void DisconnectFromDataBase(QString nameDb = "");
+    void RequestToDB(QString request);
+    QSqlError GetLastError(void);
+    void ConnectToDataBase(QVector<QString> dataForConnect);
+//==============================
+    void ReadAnswerFromDB( int answerType );
+
+signals:
+
+   //void sig_SendDataFromDB(const QTableWidget *tableWg, int typeR);
+   //void sig_SendDataFromDB(const QTableView *tableWg, int typeR);
+   // void sig_SendDataFromDB(const QTableView *tableWg);
+    void sig_SendDataFromDB(const QAbstractItemModel *model);
+
+   void sig_SendStatusConnection(bool);
+//================================
+   void sig_SendStatusRequest(QSqlError err);
+
+private:
+
+    QSqlDatabase* dataBase;
+//================================
+    //QSqlQuery* simpleQuery;
+    //QTableWidget* tableWidget;
+    QTableView* tableView;
+    QSqlQueryModel simpleQueryModel;
+    QSqlTableModel tableModel;
+};
+
+#endif // DATABASE_H
---
+++ mainwindow.h
@@ -0,0 +1,50 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QMessageBox>
+#include <QtConcurrent>
+#include "database.h"
+#include "dbdata.h"
+
+
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+
+public slots:
+    //void ScreenDataFromDB(const QTableWidget *widget, int typeRequest);
+    void ScreenDataFromDB(QAbstractItemModel *model);
+    void ReceiveStatusConnectionToDB(bool status);
+    //void ReceiveStatusRequestToDB(QSqlError err, QAbstractItemModel* model);
+    void ReceiveStatusRequestToDB(QSqlError err);
+
+private slots:
+    void on_act_addData_triggered();
+    void on_act_connect_triggered();
+    void on_pb_request_clicked();
+
+
+signals:
+    void sig_RequestToDb(QString request);
+
+private:
+
+    QVector<QString> dataForConnect; //Данные для подключения к БД.
+
+    Ui::MainWindow *ui;
+    DbData *dataDb;
+    DataBase* dataBase;
+    QMessageBox* msg;
+};
+#endif // MAINWINDOW_H
